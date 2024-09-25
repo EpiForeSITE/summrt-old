8d66e67703b45e19b77e69ef08bdfa6cdab95b61
Kaitlyn Johnson <kej38@georgetown.edu>
2024-09-25 14:46:24 -0400

    wip tests

--- inst/tinytest/test_summarize_rtestimate.R
+++ inst/tinytest/test_summarize_rtestimate.R
@@ -1,4 +1,7 @@
 # Example data 
-system.file("extdata", "EpiNow2_example.rds",
-            package = "summrt"
+EpiNow2_obj <- readRDS(
+  system.file("extdata", "EpiNow2_example.rds",
+            package = "summrt")
 )
+
+summarize_rtestimate(EpiNow2_obj)
