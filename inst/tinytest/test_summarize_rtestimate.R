518cb2fe928ae25e6eee0417cc40f54dfa37359a
Kaitlyn Johnson <kej38@georgetown.edu>
2024-09-25 15:17:33 -0400

    add basic tests for EpiNow2

--- inst/tinytest/test_summarize_rtestimate.R
+++ inst/tinytest/test_summarize_rtestimate.R
@@ -4,4 +4,24 @@ EpiNow2_obj <- readRDS(
             package = "summrt")
 )
 
-summarize_rtestimate(EpiNow2_obj)
+std_epinow2 <- summarize_rtestimate(EpiNow2_obj)
+
+message("Check that names of EpiNow2 are correct")
+checkmate::expect_names( names(std_epinow2), 
+                         must.include = c("estimates", 
+                                          "package", 
+                                          "notes"))
+
+message("Check that the date column is actually an integer")
+expect_true(is.integer(std_epinow2$estimates$date))
+
+message("Check that the package name is correct for EpiNow2")
+expect_equal(std_epinow2$package, "EpiNow2")
+
+message("Check that there are no NAs in median, lbs, ubs")
+expect_true(all(!is.na(std_epinow2$estimates$median)))
+expect_true(all(!is.na(std_epinow2$estimates$lb)))
+expect_true(all(!is.na(std_epinow2$estimates$ub)))
+
+message("Check ")
+
