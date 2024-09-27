bdbbb2eb7544552fdfc480cb03314be00aeda31b
Daniel J. McDonald <dajmcdon@gmail.com>
2024-09-27 14:31:10 -0700

    refactor probs_to_char utility

--- R/utils.R
+++ R/utils.R
@@ -7,18 +7,13 @@ level_to_probs <- function(level) {
 
 probs_to_char <- function(prob, digits = 3L,
                           remove_trailing_zero = TRUE,
-                          add_leading_zero = TRUE) {
+                          remove_leading_zero = FALSE) {
   checkmate::assert_number(prob, lower = 0, upper = 1)
   checkmate::assert_int(digits, lower = 0)
   checkmate::assert_logical(remove_trailing_zero, len = 1L)
-  checkmate::assert_logical(add_leading_zero, len = 1L)
-  .prob <- as.character(round(prob * 10^digits, 0))
-  if (nchar(.prob) < digits) {
-    .prob <- paste0(paste0(rep("0", digits -  nchar(.prob)), collapse = ""), .prob)
-  }
-  if (remove_trailing_zero) {
-    .prob <- sub("(0+)$", "", .prob)
-  }
-  .prob <- ifelse(add_leading_zero, paste0("0.", .prob), paste0(".", .prob))
+  checkmate::assert_logical(remove_leading_zero, len = 1L)
+  .prob <- sprintf(paste0("%.", digits, "f"), prob)
+  if (remove_trailing_zero) .prob <- sub("(0+)$", "", .prob)
+  if (remove_leading_zero) .prob <- sub("^0", "", .prob)
   .prob
 }
