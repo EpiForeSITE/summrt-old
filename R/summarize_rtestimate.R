ec07508cf0bca0529ffdd13aeb2e5976b0f339a5
Daniel J. McDonald <dajmcdon@gmail.com>
2024-09-27 13:19:59 -0700

    add level argument

--- R/summarize_rtestimate.R
+++ R/summarize_rtestimate.R
@@ -6,6 +6,7 @@
 #' @param median Double vector. vector of median values.
 #' @param lb Double vector. vector of lower bounds.
 #' @param ub Double vector. vector of upper bounds.
+#' @param level Double scalar. the confidence level associated with `lb`/`ub`
 #' @param package String. Name of the package.
 #' @param notes String. Notes about the summary.
 #' @export
@@ -15,10 +16,11 @@
 #'   - `median`: Double vector. vector of median values.
 #'   - `lb`: Double vector. vector of lower bounds.
 #'   - `ub`: Double vector. vector of upper bounds.
+#' - `level`: Double scalar.
 #' - `package`: String. Name of the package.
 #' - `notes`: String. Notes about the summary.
 new_summrt <- function(
-    date, median, lb, ub, package, notes
+    date, median, lb, ub, level, package, notes
 ) {
 
   # Asserting the types
@@ -26,6 +28,7 @@ new_summrt <- function(
   checkmate::assert_double(median)
   checkmate::assert_double(lb)
   checkmate::assert_double(ub)
+  checkmate::assert_number(level, lower = 0, upper = 1)
   checkmate::assert_string(package)
   checkmate::assert_string(notes)
 
@@ -46,6 +49,7 @@ new_summrt <- function(
         lb = lb,
         ub = ub
       ),
+      level = level,
       package = package,
       notes = notes
     ),
@@ -59,25 +63,29 @@ new_summrt <- function(
 #' @param ... Additional arguments passed to methods.
 print.summrt_summary <- function(x, ...) {
   cat("Summary of Rt estimation\n")
+  cat("Confidence level : ", x$level, "\n")
   cat("Package : ", x$package, "\n")
-  cat("Notes   : ", x$notes, "\n")
+  if (nchar(x$notes) > 0L) cat("Notes   : ", x$notes, "\n")
   print(x$estimates)
   invisible(x)
 }
 
 #' Extract Rt estimation from a model fit
 #' @param x Object to extract Rt from.
+#' @param level Desired confidence level for the estimate
 #' @param ... Additional arguments passed to methods.
 #' @param notes String. Optional notes to add to the summary.
 #' @export
-summarize_rtestimate <- function(x, ..., notes = "") {
+summarize_rtestimate <- function(x, level = 0.95, ..., notes = "") {
+  checkmate::assert_number(level, lower = 0, upper = 1)
+  checkmate::assert_string(notes)
   UseMethod("summarize_rtestimate")
 }
 
 #' @rdname summarize_rtestimate
 #' @importFrom cli cli_abort
 #' @export
-summarize_rtestimate.default <- function(x, ..., notes = "") {
+summarize_rtestimate.default <- function(x, level = 0.95, ..., notes = "") {
   cli::cli_abort("Your Rt method isn't supported yet. You should create a method.")
 }
 
@@ -100,7 +108,6 @@ summarize_rtestimate.cv_poisson_rt <- function(
   } else {
     checkmate::assert_number(lambda, lower = 0)
   }
-  checkmate::assert_number(level, lower = 0, upper = 1)
   cb <- rtestim::confband(x, lambda = lambda, level = level, ...)
 
   new_summrt(
@@ -108,6 +115,7 @@ summarize_rtestimate.cv_poisson_rt <- function(
     median = cb$fit,
     lb = cb[[2]], # danger
     ub = cb[[3]],
+    level = level,
     package = "rtestim",
     notes = notes
   )
@@ -126,7 +134,6 @@ summarize_rtestimate.poisson_rt <- function(x, level = 0.95, lambda = NULL, ...,
     lambda <- 10^stats::median(log10(x$lambda))
   }
   checkmate::assert_number(lambda, lower = 0)
-  checkmate::assert_number(level, lower = 0, upper = 1)
   cb <- rtestim::confband(x, lambda = lambda, level = level, ...)
 
   new_summrt(
@@ -134,6 +141,7 @@ summarize_rtestimate.poisson_rt <- function(x, level = 0.95, lambda = NULL, ...,
     median = cb$fit,
     lb = cb[[2]],
     ub = cb[[3]],
+    level = level,
     package = "rtestim",
     notes = notes
   )
@@ -154,11 +162,14 @@ summarize_rtestimate.epinow <- function(x, level = 0.95, ..., notes = "") {
   t_length <- as.integer(t_max - t_min)
   t_length_forecast <- ncol(rstan::extract(x$estimates$fit)$R) - nrow(x$estimates$observations)
 
+  probs <- level_to_probs(level)
+
   return(new_summrt(
     date = c(0:t_length, (t_length + 1):(t_length + t_length_forecast)),
     median = apply(y_extract, 2, stats::quantile, probs = 0.5),
-    lb = apply(y_extract, 2, stats::quantile, probs = 0.025),
-    ub = apply(y_extract, 2, stats::quantile, probs = 0.975),
+    lb = apply(y_extract, 2, stats::quantile, probs = probs$lower),
+    ub = apply(y_extract, 2, stats::quantile, probs = probs$upper),
+    level = level,
     package = "EpiNow2",
     notes = notes
   ))
@@ -166,18 +177,30 @@ summarize_rtestimate.epinow <- function(x, level = 0.95, ..., notes = "") {
 }
 
 #' @export
-#' @details The `estimate_R` method is for the `EpiEstim` package.
+#' @details The `estimate_R` method is for the `EpiEstim` package. Currently,
+#'   only levels in 50%, 90% and 95% confidence levels are allowed.
 #' @rdname summarize_rtestimate
-summarize_rtestimate.estimate_R <- function(x, ..., notes = "") {
+summarize_rtestimate.estimate_R <- function(x, level = 0.95, ..., notes = "") {
   if (!requireNamespace("EpiEstim", quietly = TRUE)) {
     cli::cli_abort("You must install the {.pkg EpiEstim} package for this functionality.")
   }
-
+  allowed_levels <- c(0.95, 0.9, 0.5)
+  if (min(abs(level - allowed_levels)) > sqrt(.Machine$double.eps)) {
+    cli::cli_abort(paste(
+      "For {.pkg EpiEstim}, allowable confidence levels are",
+      "{.val {allowed_levels}}, not {.val {level}}."
+    ))
+  }
+  probs <- level_to_probs(level)
+  probs <- lapply(probs, probs_to_char)
+  lb_name <- paste0("Quantile.", probs$lower, "(R)")
+  ub_name <- paste0("Quantile.", probs$upper, "(R)")
   new_summrt(
     date    = as.integer((x$R$t_end + x$R$t_start) / 2),
-    median  = x$R$`Median(R)`,
-    lb      = x$R$`Quantile.0.025(R)`,
-    ub      = x$R$`Quantile.0.975(R)`,
+    median  = x$R[["Median(R)"]],
+    lb      = x$R[[lb_name]],
+    ub      = x$R[[ub_name]],
+    level   = level,
     package = "EpiEstim",
     notes   = notes
   )
@@ -186,7 +209,7 @@ summarize_rtestimate.estimate_R <- function(x, ..., notes = "") {
 #' @export
 #' @details The `Rt` method is for the `EpiLPS` package.
 #' @rdname summarize_rtestimate
-summarize_rtestimate.Rt <- function(x, ..., notes = "") {
+summarize_rtestimate.Rt <- function(x, level = 0.95, ..., notes = "") {
   if (!requireNamespace("EpiLPS", quietly = TRUE)) {
     cli::cli_abort("You must install the {.pkg EpiLPS} package for this functionality.")
   }
@@ -196,6 +219,7 @@ summarize_rtestimate.Rt <- function(x, ..., notes = "") {
     median  = x$RLPS$Rq0.50,
     lb      = x$RLPS$Rq0.025,
     ub      = x$RLPS$Rq0.975,
+    level   = level,
     package = "EpiLPS",
     notes   = notes
   )
